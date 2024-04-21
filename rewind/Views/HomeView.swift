2cc6fb377167961dfd3a8f71b43a03fcedb95840
george-cxue <georgexuechang@gmail.com>
2024-04-21 17:12:38 -0400

    added some views

---
+++ rewind/Views/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  rewind
+//
+//  Created by George Xue on 4/21/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
