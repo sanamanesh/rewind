63fdf2f7c186e50005bca980e334b829039b6978
nandini-swami <nandini.swami.18@gmail.com>
2024-04-21 17:52:54 -0400

    implemented basic tabView

--- rewind/Views/ContentView.swift
+++ rewind/Views/ContentView.swift
@@ -6,11 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct ContentView {
+struct ContentView : View {
     var body: some View {
         TabView {
-            
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            AddView()
+                .tabItem {
+                    Label("Add", systemImage: "plus.circle")
+                }
+            MapView()
+                .tabItem {
+                    Label("Map", systemImage: "map")
+                }
         }
     }
+}
+
+#Preview {
+    ContentView()
 }
