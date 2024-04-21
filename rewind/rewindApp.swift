937c2cc5b0bbf6b83a3ad6b5ccad5b0238d9d038
sanamanesh <sanmanesh@gmail.com>
2024-04-21 16:31:03 -0400

    Initial Commit

---
+++ rewind/rewindApp.swift
@@ -0,0 +1,17 @@
+//
+//  rewindApp.swift
+//  rewind
+//
+//  Created by Sana Manesh on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct rewindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
