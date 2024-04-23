7eb8230e6a4533fac66ce9e3b0c2cb5c998fdf84
nandini-swami <nandini.swami.18@gmail.com>
2024-04-23 01:29:48 -0400

    created base addview

--- rewind/Views/AddView.swift
+++ rewind/Views/AddView.swift
@@ -8,8 +8,62 @@
 import SwiftUI
 
 struct AddView: View {
+    @State private var placeName = ""
+    @State private var location = ""
+    @State private var rating = 0
+    @State private var description = ""
     var body: some View {
-        Text("Inside App View!")
+        VStack {
+            TextField("Name of the Place", text: $placeName)
+                .padding()
+            TextField("Location", text: $location)
+                .padding()
+            Stepper(value: $rating, in: 0...5) {
+                RatingView(rating: $rating)
+            }
+            .padding()
+            TextEditor(text: $description)
+                .frame(height: 200)
+                .padding()
+
+            Spacer()
+
+            Button(action: {
+                // Add button action
+                // Here you can implement the logic to add the data to your model or database
+                // For example, you can create a Place object with the entered data and save it
+            }) {
+                Text("Add")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+/// Custom View for displaying stars based on the rating and showing the number of stars
+struct RatingView: View {
+    @Binding var rating: Int
+
+    private func starType(index: Int) -> String {
+        return index <= rating ? "star.fill" : "star"
+    }
+
+    var body: some View {
+        HStack {
+            ForEach(1...5, id: \.self) { index in
+                Image(systemName: starType(index: index))
+                    .foregroundColor(Color.yellow)
+            }
+            Text("\(rating)/5")
+                .foregroundColor(.gray)
+                .font(.caption)
+        }
     }
 }
 
