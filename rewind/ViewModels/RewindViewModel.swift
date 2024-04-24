12be9c00cd8de22861cf95c02db6d7d479e9654e
sceline <sanamanesh@gmail.com>
2024-04-24 15:43:27 -0400

    first test of places api

--- rewind/ViewModels/RewindViewModel.swift
+++ rewind/ViewModels/RewindViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class RewindViewModel: ObservableObject {
     @Published var cards: [Card] = []
-    @Published var loc: Location?
+    @Published var loc: Coord?
     @Published var locString: String = ""
     
 //    init(){
@@ -22,25 +22,41 @@ class RewindViewModel: ObservableObject {
         print("updating currLoc")
         
         locString = currLocString
+        
+        let apiCalls = APICalls() // Create an instance of APICalls
+            
         do {
-            if let location = try? await APICalls.instance.getLocation(city: locString) {
-                    self.loc = location.first
-            } else {
-                // Handle case where API call returns nil (or encounters an error)
-                print("LOCATION - Error or nil result from API call")
+            try await apiCalls.getPlaceCoordinates(storeName: currLocString) { result in
+                switch result {
+                case .success(let coord):
+                    self.loc = coord
+                    print("Latitude: \(coord.lat), Longitude: \(coord.lng)")
+                case .failure(let error):
+                    print("Error:", error)
+                }
             }
+        } catch {
+            print("Error:", error)
         }
-        print("set the current location to \(String(describing: loc))")
+//        do {
+//            if let location = try? await APICalls.instance.getLocation(city: locString) {
+//                    self.loc = location.first
+//            } else {
+//                // Handle case where API call returns nil (or encounters an error)
+//                print("LOCATION - Error or nil result from API call")
+//            }
+//        }
+//        print("set the current location to \(String(describing: loc))")
     }
     
-    func addCard(name: String, description: String, rating: Int, location: Location?) {
+    func addCard(name: String, description: String, rating: Int, location: Coord?) {
         let location = loc;
         print("made it to adding a card")
         let newCard = Card(name: name, location: location, description: description, rating: rating)
         DispatchQueue.main.async {
             self.cards.append(newCard)
             print(self.cards.count)
-            print("date: \(self.cards[0].date), description: \(self.cards[0].description), name: \(self.cards[0].name), loc name: \(String(describing: self.cards[0].location?.display_name))")
+            print("date: \(self.cards[0].date), description: \(self.cards[0].description), name: \(self.cards[0].name), lat: \(String(describing: self.cards[0].location?.lat)), long: \(String(describing: self.cards[0].location?.lng))")
         }
         
         
