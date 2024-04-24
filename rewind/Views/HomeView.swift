b02c57cbcdaf9dc391b1e8a54d1151a3f6801770
nandini-swami <nandini.swami.18@gmail.com>
2024-04-24 15:29:24 -0400

    messing with card and home view

--- rewind/Views/HomeView.swift
+++ rewind/Views/HomeView.swift
@@ -11,21 +11,28 @@ struct HomeView: View {
     @EnvironmentObject var rewindViewModel: RewindViewModel
     @State private var showingSheet = false
     
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+        // Add more GridItems if you want more columns
+    ]
+    
     var body: some View {
-        
-        Text("Inside Home View!")
-        
-        let testCard = Card(name: "Huntsman", description: "Wharton School", rating: 5)
-        
-        Button("Test Card") {
-            showingSheet = true // Toggle the boolean to show the sheet
-        }
-        .buttonStyle(.borderedProminent)
-        .controlSize(.large)
-        .padding()
-        .sheet(isPresented: $showingSheet) { // Use the boolean state directly
-            CardView(card: testCard) // Pass the instance of Card directly
-        }
+        NavigationView {
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 20) {
+                            ForEach(rewindViewModel.cards) { card in
+                                CardView(card: card)
+                                    .onTapGesture {
+                                        // Here you can define what happens when a card is tapped.
+                                        // For example, you can set showingSheet to true and show details in a sheet.
+                                    }
+                            }
+                        }
+                        .padding()
+                    }
+                    .navigationTitle("REWIND IN PHILLY")
+                }
     }
 }
 
